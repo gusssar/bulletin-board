fddc87b6ec1a241a13e762238d543bd2904f52ae
gusssar <dassler47@gmail.com>
2019-02-03 21:18:49 +0300

    update README

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from '../../logo.svg';
 import './index.css';
 import AdSubmit from '../AdSubmit/index.js';
 import AdList from '../AdList/index.js';
